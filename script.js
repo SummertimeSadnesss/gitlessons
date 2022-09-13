e275f499eae752f5a74f015fb5b928d0206658fe
Andrew L <phlike3@gmail.com>
2022-09-14 00:33:38 +0400

    =>

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const name = 'Андрей';
 const city = 'Астрахань';
 
 console.log(`Меня зовут ${name}, я живу городе ${city}`);
+
+
+
+console.log((a, b) => a + b);
+
+myFunc1 = function(value, multiplier = 1) {
+    return value * multiplier;
+}
+
+console.log(myFunc1(5));
+
+myFunc2 = (value, multiplier = 1) => {
+    return value * multiplier;
+}
+
+console.log(myFunc2(6));
