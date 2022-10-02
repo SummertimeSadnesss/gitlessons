7bff72e535bc4915ad0ad95fb83e4eb917979ca9
Andrew L <phlike3@gmail.com>
2022-10-02 21:08:15 +0400

    react,modules

---
+++ modules/moduleOne.mjs
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+const umn = (a, b) => a * b;
+
+export { sum, umn };
+
+
+
+
+
---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,12 @@
+import {
+    sum as sum41,
+    umn as umn41
+} from './moduleOne.mjs'
+
+const res1 = sum41(5, 5);
+
+console.log(res1);
+
+const res2 = umn41(1, 100);
+
+console.log(res2);
