f5dad491fe855228e5d1627ec6f617ffccf96adf
Andrey <phlike3@gmail.com>
2022-11-11 11:12:41 +0400

    Create DB movies

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('Закрыть?')
+
+
+
+const numberOfFilms = prompt('Сколько фильмов уже посмотрели?', 'Введите количество фильмов');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+const lastFilms = prompt('Один из последних просмотренных фильмов?', 'Введите название фильма');
+const rateFilms = prompt('На сколько цените его? (по 10-ти бальной шкале)', '10');
+
+personalMovieDB.count = +numberOfFilms;
+personalMovieDB.movies[lastFilms] = +rateFilms;
+
+console.log(personalMovieDB);
