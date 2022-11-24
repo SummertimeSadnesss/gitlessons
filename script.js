2647d4c26ace12051a4dc1198ea0adc05f1de3fd
Andrey <phlike3@gmail.com>
2022-11-24 16:54:48 +0400

    massive

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+//
+// // Место для первой задачи
+// function firstTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+//
+//     // Пишем решение вот тут
+//     arr.forEach(elem => result.push(elem))
+//
+//     // Не трогаем
+//     return result;
+// }
 
-let result = '';
+// // Место для второй задачи
+// function secondTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//
+//     // Пишем решение вот тут
+//     data.forEach((elem, index) => {
+//         if (typeof elem === 'string') {
+//             data[index] = elem + ' - done';
+//         } else {
+//             data[index] = elem * 2;
+//         }
+//     })
+//
+//     // Не трогаем
+//     return data;
+// }
 
 
-for (let i = 0; i < 3; i++) {
-    for (let i = 0; i <= 1; i++) {
-        result += '*';
-    }
-}
-
-console.log(result);
-
+// // Место для третьей задачи
+// function thirdTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+//
+//     // Пишем решение вот тут
+//     data.forEach((elem) => result.push(elem));
+//     result.reverse();
+//
+//     // Не трогаем
+//     return result;
+// }
